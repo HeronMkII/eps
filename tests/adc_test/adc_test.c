e48729c699d1b21e228e807cbf52f5400c3399aa
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-06-24 23:16:43 -0400

    Update adc_test for verification testing

--- tests/adc_test/adc_test.c
+++ tests/adc_test/adc_test.c
@@ -6,22 +6,23 @@
 #include <adc/adc.h>
 #include <spi/spi.h>
 #include <util/delay.h>
+#include <conversions/conversions.h>
 
 
 void read_voltage(uint8_t channel) {
     uint16_t raw_data = adc_read_channel_raw_data(channel);
     double raw_voltage = adc_convert_raw_data_to_raw_voltage(raw_data);
     double voltage = adc_eps_convert_raw_voltage_to_voltage(raw_voltage);
-    print("Channel: %u, Raw Data: 0x%04x, Raw Voltage: %d mV, Voltage: %d mV\n\n",
-            channel, raw_data, (int16_t) (raw_voltage * 1000.0), (int16_t) (voltage * 1000.0));
+    print("Channel: %u, Raw Data: 0x%04x, Raw Voltage: %ld mV, Voltage: %ld mV\n\n",
+            channel, raw_data, (int32_t) (raw_voltage * 1000.0), (int32_t) (voltage * 1000.0));
 }
 
 void read_current(uint8_t channel) {
     uint16_t raw_data = adc_read_channel_raw_data(channel);
     double raw_voltage = adc_convert_raw_data_to_raw_voltage(raw_data);
     double current = adc_eps_convert_raw_voltage_to_current(raw_voltage);
-    print("Channel: %u, Raw Data: 0x%04x, Raw Voltage: %d mV, Current: %d mA\n\n",
-            channel, raw_data, (int16_t) (raw_voltage * 1000.0), (int16_t) (current * 1000.0));
+    print("Channel: %u, Raw Data: 0x%04x, Raw Voltage: %ld mV, Current: %ld mA\n\n",
+            channel, raw_data, (int32_t) (raw_voltage * 1000.0), (int32_t) (current * 1000.0));
 }
 
 
@@ -34,8 +35,7 @@ int main(void) {
     init_spi();
     print("SPI Initialized\n");
 
-    adc_init_constants(ADC_EPS);
-    adc_init();
+    adc_init(ADC_EPS);
     print("ADC Initialized\n\n");
 
     print("Starting test\n\n");
@@ -50,23 +50,23 @@ int main(void) {
         print("ADC_EPS_BATT_VNEG_CH\n");
         read_voltage(ADC_EPS_BATT_VNEG_CH);
 
-        print("ADC_EPS_BB1_IOUT_CH\n");
-        read_current(ADC_EPS_BB1_IOUT_CH);
+        // print("ADC_EPS_BB1_IOUT_CH\n");
+        // read_current(ADC_EPS_BB1_IOUT_CH);
         print("ADC_EPS_BB2_IOUT_CH\n");
         read_current(ADC_EPS_BB2_IOUT_CH);
-        print("ADC_EPS_BT1_IOUT_CH\n");
-        read_current(ADC_EPS_BT1_IOUT_CH);
+        // print("ADC_EPS_BT1_IOUT_CH\n");
+        // read_current(ADC_EPS_BT1_IOUT_CH);
         print("ADC_EPS_BATT_IOUT_CH\n");
         read_current(ADC_EPS_BATT_IOUT_CH);
 
-        print("ADC_EPS_PV_POS_X_IOUT_CH\n");
-        read_current(ADC_EPS_PV_POS_X_IOUT_CH);
-        print("ADC_EPS_PV_NEG_X_IOUT_CH\n");
-        read_current(ADC_EPS_PV_NEG_X_IOUT_CH);
-        print("ADC_EPS_PV_POS_Y_IOUT_CH\n");
-        read_current(ADC_EPS_PV_POS_Y_IOUT_CH);
-        print("ADC_EPS_PV_NEG_Y_IOUT_CH\n");
-        read_current(ADC_EPS_PV_NEG_Y_IOUT_CH);
+        // print("ADC_EPS_PV_POS_X_IOUT_CH\n");
+        // read_current(ADC_EPS_PV_POS_X_IOUT_CH);
+        // print("ADC_EPS_PV_NEG_X_IOUT_CH\n");
+        // read_current(ADC_EPS_PV_NEG_X_IOUT_CH);
+        // print("ADC_EPS_PV_POS_Y_IOUT_CH\n");
+        // read_current(ADC_EPS_PV_POS_Y_IOUT_CH);
+        // print("ADC_EPS_PV_NEG_Y_IOUT_CH\n");
+        // read_current(ADC_EPS_PV_NEG_Y_IOUT_CH);
 
         print("\n");
         _delay_ms(5000);
