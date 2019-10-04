85134924502fe22cd1c02865fefa21588e8677d9
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-10-03 21:23:40 -0400

    Refactor command handling for updated constants

--- src/can_commands.c
+++ src/can_commands.c
@@ -29,7 +29,7 @@ void handle_rx_msg(void) {
         dequeue(&can_rx_msg_queue, rx_msg);
     }
 
-    uint8_t msg_type = rx_msg[2];
+    uint8_t opcode = rx_msg[2];
     uint8_t field_num = rx_msg[3];
     uint32_t rx_data =
         ((uint32_t) rx_msg[4] << 24) |
@@ -37,7 +37,7 @@ void handle_rx_msg(void) {
         ((uint32_t) rx_msg[6] << 8) |
         ((uint32_t) rx_msg[7]);
 
-    switch (msg_type) {
+    switch (opcode) {
         case CAN_EPS_HK:
             handle_rx_hk(field_num);
             break;
@@ -52,12 +52,61 @@ void handle_rx_msg(void) {
 void handle_rx_hk(uint8_t field_num) {
     uint32_t tx_data = 0;
 
+    // TODO - field implementations
+
     // Check field number
-    if ((CAN_EPS_HK_BAT_VOL <= field_num) &&
-            (field_num <= CAN_EPS_HK_PAY_CON_TEMP)) {
-        uint8_t channel = field_num - CAN_EPS_HK_BAT_VOL;
-        fetch_adc_channel(&adc, channel);
-        tx_data = read_adc_channel(&adc, channel);
+    if (field_num == CAN_EPS_HK_BAT_VOL) {
+        // uint8_t channel = field_num - CAN_EPS_HK_BAT_VOL;
+        // fetch_adc_channel(&adc, channel);
+        // tx_data = read_adc_channel(&adc, channel);
+    }
+
+    else if (field_num == CAN_EPS_HK_BAT_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_X_POS_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_X_NEG_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_Y_POS_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_Y_NEG_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_3V3_VOL) {
+    }
+
+    else if (field_num == CAN_EPS_HK_3V3_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_5V_VOL) {
+    }
+
+    else if (field_num == CAN_EPS_HK_5V_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_PAY_CUR) {
+    }
+
+    else if (field_num == CAN_EPS_HK_BAT_TEMP1) {
+    }
+
+    else if (field_num == CAN_EPS_HK_BAT_TEMP2) {
+    }
+
+    else if (field_num == CAN_EPS_HK_3V3_TEMP) {
+    }
+
+    else if (field_num == CAN_EPS_HK_5V_TEMP) {
+    }
+
+    else if (field_num == CAN_EPS_HK_PAY_CON_TEMP) {
+    }
+
+    else if (field_num == CAN_EPS_HK_SHUNTS) {
     }
 
     else if (field_num == CAN_EPS_HK_HEAT1_SP) {
@@ -66,56 +115,54 @@ void handle_rx_hk(uint8_t field_num) {
 
     else if (field_num == CAN_EPS_HK_HEAT2_SP) {
         tx_data = dac.raw_voltage_b;
+    }    
+
+    else if (field_num == CAN_EPS_HK_GYR_UNCAL_X) {
+        uint16_t uncal_x = 0;
+        get_imu_uncal_gyro(&uncal_x, NULL, NULL, NULL, NULL, NULL);
+        tx_data = (uint32_t) uncal_x;
     }
 
-    else if ((CAN_EPS_HK_GYR_UNCAL_X <= field_num) &&
-            (field_num <= CAN_EPS_HK_GYR_UNCAL_Z)) {
-        uint16_t uncal_x = 0, uncal_y = 0, uncal_z = 0;
-        get_imu_uncal_gyro(&uncal_x, &uncal_y, &uncal_z, NULL, NULL, NULL);
-
-        switch (field_num) {
-            case CAN_EPS_HK_GYR_UNCAL_X:
-                tx_data = (uint32_t) uncal_x;
-                break;
-            case CAN_EPS_HK_GYR_UNCAL_Y:
-                tx_data = (uint32_t) uncal_y;
-                break;
-            case CAN_EPS_HK_GYR_UNCAL_Z:
-                tx_data = (uint32_t) uncal_z;
-                break;
-            default:
-                break;
-        }
+    else if (field_num == CAN_EPS_HK_GYR_UNCAL_Y) {
+        uint16_t uncal_y = 0;
+        get_imu_uncal_gyro(NULL, &uncal_y, NULL, NULL, NULL, NULL);
+        tx_data = (uint32_t) uncal_y;
     }
 
-    else if ((CAN_EPS_HK_GYR_CAL_X <= field_num) &&
-            (field_num <= CAN_EPS_HK_GYR_CAL_Z)) {
-        uint16_t cal_x = 0, cal_y = 0, cal_z = 0;
-        get_imu_cal_gyro(&cal_x, &cal_y, &cal_z);
+    else if (field_num == CAN_EPS_HK_GYR_UNCAL_Z) {
+        uint16_t uncal_z = 0;
+        get_imu_uncal_gyro(NULL, NULL, &uncal_z, NULL, NULL, NULL);
+        tx_data = (uint32_t) uncal_z;
+    }
 
-        switch (field_num) {
-            case CAN_EPS_HK_GYR_CAL_X:
-                tx_data = (uint32_t) cal_x;
-                break;
-            case CAN_EPS_HK_GYR_CAL_Y:
-                tx_data = (uint32_t) cal_y;
-                break;
-            case CAN_EPS_HK_GYR_CAL_Z:
-                tx_data = (uint32_t) cal_z;
-                break;
-            default:
-                break;
-        }
+    else if (field_num == CAN_EPS_HK_GYR_CAL_X) {
+        uint16_t cal_x = 0;
+        get_imu_cal_gyro(&cal_x, NULL, NULL);
+        tx_data = (uint32_t) cal_x;
+    }
+
+    else if (field_num == CAN_EPS_HK_GYR_CAL_Y) {
+        uint16_t cal_y = 0;
+        get_imu_cal_gyro(NULL, &cal_y, NULL);
+        tx_data = (uint32_t) cal_y;
+    }
+
+    else if (field_num == CAN_EPS_HK_GYR_CAL_Z) {
+        uint16_t cal_z = 0;
+        get_imu_cal_gyro(NULL, NULL, &cal_z);
+        tx_data = (uint32_t) cal_z;
+    }
+
+    else if (field_num == CAN_EPS_HK_UPTIME) {
+        tx_data = uptime_s;
     }
 
     else if (field_num == CAN_EPS_HK_RESTART_COUNT) {
         tx_data = restart_count;
     }
+
     else if (field_num == CAN_EPS_HK_RESTART_REASON) {
         tx_data = restart_reason;
-    }
-    else if (field_num == CAN_EPS_HK_UPTIME) {
-        tx_data = uptime_s;
     }
 
     // If the message type is not recognized, return before enqueueing
@@ -144,6 +191,8 @@ void handle_rx_hk(uint8_t field_num) {
 void handle_rx_ctrl(uint8_t field_num, uint32_t rx_data) {
     uint32_t tx_data = 0;
 
+    // TODO - field implementations
+
     // Check field number
 
     if (field_num == CAN_EPS_CTRL_PING) {
@@ -151,22 +200,45 @@ void handle_rx_ctrl(uint8_t field_num, uint32_t rx_data) {
         // Just take care of the condition so we don't return early below
     }
     
+    else if (field_num == CAN_EPS_CTRL_GET_HEAT1_SHAD_SP) {
+    }
+
     else if (field_num == CAN_EPS_CTRL_SET_HEAT1_SHAD_SP) {
         set_raw_heater_setpoint(&heater_1_shadow_setpoint, (uint16_t) rx_data);
     }
+
+    else if (field_num == CAN_EPS_CTRL_GET_HEAT2_SHAD_SP) {
+    }
+
     else if (field_num == CAN_EPS_CTRL_SET_HEAT2_SHAD_SP) {
         set_raw_heater_setpoint(&heater_2_shadow_setpoint, (uint16_t) rx_data);
     }
+
+    else if (field_num == CAN_EPS_CTRL_GET_HEAT1_SUN_SP) {
+    }
+
     else if (field_num == CAN_EPS_CTRL_SET_HEAT1_SUN_SP) {
         set_raw_heater_setpoint(&heater_1_sun_setpoint, (uint16_t) rx_data);
     }
+
+    else if (field_num == CAN_EPS_CTRL_GET_HEAT2_SUN_SP) {
+    }
+
     else if (field_num == CAN_EPS_CTRL_SET_HEAT2_SUN_SP) {
         set_raw_heater_setpoint(&heater_2_sun_setpoint, (uint16_t) rx_data);
+    }
+
+    // TODO - rename current constants in lib-common
+    else if (field_num == CAN_EPS_CTRL_GET_HEAT_CUR_THRESH_LOWER) {
     }
 
     else if (field_num == CAN_EPS_CTRL_SET_HEAT_CUR_THRESH_LOWER) {
         set_raw_heater_cur_thresh(&heater_sun_cur_thresh_lower, (uint16_t) rx_data);
     }
+
+    else if (field_num == CAN_EPS_CTRL_GET_HEAT_CUR_THRESH_UPPER) {
+    }
+
     else if (field_num == CAN_EPS_CTRL_SET_HEAT_CUR_THRESH_UPPER) {
         set_raw_heater_cur_thresh(&heater_sun_cur_thresh_upper, (uint16_t) rx_data);
     }
@@ -183,6 +255,27 @@ void handle_rx_ctrl(uint8_t field_num, uint32_t rx_data) {
         // warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
         uint32_t* ptr = (uint32_t*) ((uint16_t) rx_data);
         tx_data = eeprom_read_dword(ptr);
+    }
+
+    else if (field_num == CAN_EPS_CTRL_ERASE_EEPROM) {
+        // Received rx_data as a uint32_t but need to represent it as a uint32_t*, which the eeprom function requires
+        // Note that sizeof(uint32_t) = 4, sizeof(uint32_t*) = 2
+        // Need to case to uint16_T first or else we get
+        // warning: cast to pointer from integer of different size [-Wint-to-pointer-cast]
+        uint32_t* ptr = (uint32_t*) ((uint16_t) rx_data);
+        eeprom_update_dword(ptr, EEPROM_DEF_DWORD);
+    }
+
+    else if (field_num == CAN_EPS_CTRL_READ_RAM_BYTE) {
+    }
+
+    else if (field_num == CAN_EPS_CTRL_START_TEMP_LPM) {
+    }
+
+    else if (field_num == CAN_EPS_CTRL_ENABLE_INDEF_LPM) {
+    }
+
+    else if (field_num == CAN_EPS_CTRL_DISABLE_INDEF_LPM) {
     }
 
     // If the field number is not recognized, return before enqueueing so we
