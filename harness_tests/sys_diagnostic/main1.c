5c254ccfa945a264bbaee0026476bacdb1130a3a
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2020-02-08 14:31:20 -0500

    Fix errors and update heartbeat implementation in system diagnostic

--- harness_tests/sys_diagnostic/main1.c
+++ harness_tests/sys_diagnostic/main1.c
@@ -8,36 +8,44 @@
 void pay_reset_test(void){
     uint32_t stale_num_restarts = 0x00;
 
-    hb_send_pay_req = true;
+    pay_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
-    stale_num_restarts = hb_latest_restart_count;
+    _delay_ms(100);
+    stale_num_restarts = pay_hb_dev.restart_count;
 
-    send_hb_reset(HB_PAY);
+    send_hb_reset((hb_dev_t*) &pay_hb_dev);
 
     _delay_ms(1000);
-    hb_send_pay_req = true;
+    pay_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
+    _delay_ms(100);
 
-    ASSERT_EQ(hb_latest_restart_count, stale_num_restarts + 1);
-    ASSERT_EQ(hb_latest_restart_reason, UPTIME_RESTART_REASON_EXTRF);
+    ASSERT_EQ(pay_hb_dev.restart_count, stale_num_restarts + 1);
+    ASSERT_EQ(pay_hb_dev.restart_reason, UPTIME_RESTART_REASON_EXTRF);
 }
 
 /* Resets the OBC SSM and verifies that the reset counter increments correctly */
 void obc_reset_test(void){
     uint32_t stale_num_restarts = 0x00;
 
-    hb_send_obc_req = true;
+    obc_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
-    stale_num_restarts = hb_latest_restart_count;
+    _delay_ms(100);
+    stale_num_restarts = obc_hb_dev.restart_count;
 
-    send_hb_reset(HB_OBC);
+    send_hb_reset((hb_dev_t*) &obc_hb_dev);
 
     _delay_ms(1000);
-    hb_send_obc_req = true;
+    obc_hb_dev.send_req_flag = true;
+    _delay_ms(100);
     run_hb();
+    _delay_ms(100);
 
-    ASSERT_EQ(hb_latest_restart_count, stale_num_restarts + 1);
-    ASSERT_EQ(hb_latest_restart_reason, UPTIME_RESTART_REASON_EXTRF);
+    ASSERT_EQ(obc_hb_dev.restart_count, stale_num_restarts + 1);
+    ASSERT_EQ(obc_hb_dev.restart_reason, UPTIME_RESTART_REASON_EXTRF);
 }
 
 test_t t1 = { .name = "PAY Reset Test", .fn = pay_reset_test };
@@ -48,6 +56,6 @@ test_t* suite[] = { &t1, &t2};
 int main(void) {
     init_eps();
     init_hb(HB_EPS);
-    run_tests(suite, TEST_SIZE);
+    run_tests(suite, sizeof(suite) / sizeof(suite[0]));
     return 0;
 }
